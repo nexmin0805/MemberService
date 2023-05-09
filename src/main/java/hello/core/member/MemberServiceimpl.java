fd0a602c939bae9bf417e2cefbe3e41ab2470cf5
Min Seo Han <65328995+nexmin0805@users.noreply.github.com>
2023-05-09 19:51:57 +0900

    RateDiscountPolicy&Test

--- src/main/java/hello/core/member/MemberServiceimpl.java
+++ src/main/java/hello/core/member/MemberServiceimpl.java
@@ -5,11 +5,13 @@ public class MemberServiceimpl implements MemberService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
+
         memberRepository.save(member);
     }
 
     @Override
     public Member findMember(Long memberID) {
+
         return memberRepository.findById(memberID);
     }
 }
